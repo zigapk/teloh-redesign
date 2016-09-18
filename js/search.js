ad1dd2a05efef56a26a4d5f2a6abac9c52211ead
zigapk <ziga.patacko@gmail.com>
2016-09-18 18:48:02 +0200

    add search items

--- js/search.js
+++ js/search.js
@@ -9,13 +9,34 @@ function getUrlVars() {
     return vars;
 }
 
+function init() {
+    var speed = 250,
+        easing = mina.easeinout;
+
+    [].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
+        var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+            pathConfig = {
+                from : path.attr( 'd' ),
+                to : el.getAttribute( 'data-path-hover' )
+            };
+
+        el.addEventListener( 'mouseenter', function() {
+            path.animate( { 'path' : pathConfig.to }, speed, easing );
+        } );
+
+        el.addEventListener( 'mouseleave', function() {
+            path.animate( { 'path' : pathConfig.from }, speed, easing );
+        } );
+    } );
+}
 
 $(document).ready(function () {
+
+    init();
 
     setTimeout(function () {
         var q = getUrlVars()['q'];
         $("iframe").contents().find("#search_bar_input").val(q);
-        console.log("ready!");
     }, 1000);
 });
 
